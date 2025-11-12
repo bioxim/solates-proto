511184c7bfecf6d483b07bc55d4efdf9c7110aaa
Xime <ximecamino@gmail.com>
2025-11-11 21:29:11 -0300

    Fix: Eliminadas directivas eslint-disable no utilizadas para pasar CI check.

--- src/components/dashboard/NextQuest.tsx
+++ src/components/dashboard/NextQuest.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Play } from "lucide-react";
--- src/components/profile/EmailVerify.tsx
+++ src/components/profile/EmailVerify.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { motion } from "framer-motion";
 import { Mail } from "lucide-react";
 import { useEffect } from "react";
